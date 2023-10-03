ddbfdee81d68a4fa2120afef4c501d528f1b06f0
KimsungjoonMyLaptop <lastbabo2480@tukorea.ac.kr>
2023-10-04 00:41:29 +0900

    첫번째 점으로 이동하는 기능 구현 완료

--- Drill06.py
+++ Drill06.py
@@ -42,16 +42,16 @@ def reset_world():
     action = 3
 
     points = [ (100, 900), (1200, 800), (500, 100) ]
-    # set_new_target_arrow()
+    set_new_target_arrow()
 
 
 def set_new_target_arrow():
     global sx, sy, hx, hy, t
     global action
     global frame
-    sx, sy = cx, cy
-    # hx, hy = TUK_WIDTH - 50, TUK_HEIGHT - 50 # p1 : 시작점
-    # hx, hy = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)  # p2: 끝점
+    sx, sy = cx, cy # p1 : 시작점
+    # hx, hy = 50, 50
+    hx, hy = points[0] # p2: 끝점
     t = 0.0
     action = 1 if sx < hx else 0
     frame = 0
@@ -74,13 +74,13 @@ def update_world():
 
     frame = (frame + 1) % 8
 
-    # if t <= 1.0:
-    #     cx = (1 - t) * sx + t * hx  # cx는 시작 x와 끝 x를 1-t:t비율로 섞은 위치
-    #     cy = (1 - t) * sy + t * hy
-    #     t += 0.001
-    # else:
-    #     cx, cy = hx, hy  # 캐릭터 위치를 목적지 위치와 정확히 위치시킴
-    #     set_new_target_arrow()
+    if t <= 1.0:
+        cx = (1 - t) * sx + t * hx  # cx는 시작 x와 끝 x를 1-t:t비율로 섞은 위치
+        cy = (1 - t) * sy + t * hy
+        t += 0.001
+    else:
+        cx, cy = hx, hy  # 캐릭터 위치를 목적지 위치와 정확히 위치시킴
+        set_new_target_arrow()
 
 
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
